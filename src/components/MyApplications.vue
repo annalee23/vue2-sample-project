cfc3ae89dcce0e98cdca19b46ba8bff0b236a1cb
annalee <annalee31226@gmail.com>
2024-06-04 00:10:21 +0500

    add edit/delete orders

--- src/components/MyApplications.vue
+++ src/components/MyApplications.vue
@@ -6,10 +6,22 @@
           <h2>Список заявок</h2>
           <v-spacer></v-spacer>
           <v-btn class="my-4" color="primary" @click="openDialog()">Добавить заявку</v-btn>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title class="text-h5">Хотите удалить заявку?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text @click="closeDelete">Отмена</v-btn>
+                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-toolbar>
       </template>
+
       <template #[`item.stg`]="{ item }">
-        <span>{{ item.stg.join(', ') }}</span>
+        <span>{{ Array.isArray(item.stg) ? item.stg.join(', ') : item.stg }}</span>
       </template>
       <template #[`item.dadd`]="{ item }">
         <span>{{ formatDate(item.dadd) }}</span>
@@ -22,12 +34,20 @@
           {{ item.num }}
         </div>
       </template>
+
+      <template #[`item.actions`]="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+          mdi-delete
+        </v-icon>
+      </template>
     </v-data-table>
+
     <v-dialog v-model="dialog" max-width="600px">
       <v-card>
-        <v-card-title>
-          {{ dialogTitle }}
-        </v-card-title>
+        <v-card-title>{{ dialogTitle }}</v-card-title>
         <v-card-text>
           <v-form ref="form">
             <v-text-field v-model="editedItem.num" label="№ Заявки"></v-text-field>
@@ -63,6 +83,7 @@
   </div>
 </template>
 
+
 <script>
 import { mapMutations, mapState } from 'vuex';
 import moment from 'moment';
@@ -80,7 +101,8 @@ export default {
       { text: 'Дата', value: 'dadd' },
       { text: 'Клиент', value: 'client_name' },
       { text: 'Статус', value: 'state' },
-      { text: 'Телефон', value: 'person_phone' }
+      { text: 'Телефон', value: 'person_phone' },
+      { text: 'Действия', value: 'actions', sortable: false }
     ],
     statusOptions: [
       'init',
@@ -92,8 +114,8 @@ export default {
     editedIndex: -1,
     editedItem: {
       num: 0,
-      stg: ["ACCOPEN"],
-      dadd: null,
+      stg: [],
+      dadd: moment().format('YYYY-MM-DD'),
       client_name: "",
       state: "init",
       person_phone: ""
@@ -101,19 +123,27 @@ export default {
     defaultItem: {
       num: 0,
       stg: [],
-      dadd: "2021-06-24T14:28:14.237",
-      client_name: " ",
+      dadd: moment().format('YYYY-MM-DD'),
+      client_name: "",
       state: "init",
       person_phone: ""
     },
   }),
+  watch: {
+    dialog(val) {
+      val || this.close()
+    },
+    dialogDelete(val) {
+      val || this.closeDelete()
+    },
+  },
   computed: {
     ...mapState(['ordersList']),
   },
   methods: {
     ...mapMutations(['addNewItem']),
     formatDate(date) {
-      return moment(date).format('DD.MM.YYYY HH:mm');
+      return moment(date).isValid() ? moment(date).format('DD.MM.YYYY HH:mm') : 'Invalid date';
     },
     statusClass(status) {
       return {
@@ -137,9 +167,12 @@ export default {
     },
     saveItem() {
       if (this.$refs.form.validate()) {
-        if (this.editedItem.id) {
+        if (!moment(this.editedItem.dadd).isValid()) {
+          this.editedItem.dadd = moment().format('YYYY-MM-DD');
+        }
+        if (this.editedItem.id > -1) {
           // Редактирование существующей заявки
-          // Реализуйте логику редактирования заявки
+          Object.assign(this.ordersList[this.editedIndex], this.editedItem);
         } else {
           // Создание новой заявки
           this.addNewItem(this.editedItem);
@@ -147,11 +180,42 @@ export default {
         this.closeDialog();
       }
     },
+    editItem(item) {
+      this.editedIndex = this.ordersList.indexOf(item);
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.ordersList.indexOf(item);
+      this.editedItem = Object.assign({}, item)
+      this.dialogDelete = true
+    },
+
+    deleteItemConfirm() {
+      this.ordersList.splice(this.editedIndex, 1);
+      this.closeDelete()
+    },
+    close() {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+    closeDelete() {
+      this.dialogDelete = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
   },
   mounted() {
     this.$store.dispatch('fetchOrdersList');
   }
 };
+
 </script>
 
 <style scoped>
@@ -210,3 +274,4 @@ tr:hover {
   /* Светло-серый */
 }
 </style>
+
