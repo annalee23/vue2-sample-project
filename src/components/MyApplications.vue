7012713558a6f9bc8e20386232fb1171c73f0d0f
annalee <annalee31226@gmail.com>
2024-06-06 16:12:41 +0500

    add component DialogDelete

--- src/components/MyApplications.vue
+++ src/components/MyApplications.vue
@@ -6,17 +6,8 @@
           <h2>Список заявок</h2>
           <v-spacer></v-spacer>
           <v-btn class="my-4" color="primary" @click="openDialog()">Добавить заявку</v-btn>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="text-h5">Хотите удалить заявку?</v-card-title>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Отмена</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">Да</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
+          <DialogDelete :dialog="dialogDelete" @close="closeDelete" @confirm="deleteItemConfirm" />
+
         </v-toolbar>
       </template>
 
@@ -91,9 +82,14 @@
 <script>
 import { mapMutations, mapState } from 'vuex';
 import moment from 'moment';
+import DialogDelete from './DialogDelete.vue';
+
 
 
 export default {
+  components: {
+    DialogDelete,
+  },
   data: () => ({
     menu2: false,
     dialog: false,
