634fa8bb1fd1b65d50aad2013580266d371d0865
annalee <annalee31226@gmail.com>
2024-06-07 11:10:09 +0500

    add component DialogEditCreate

--- src/components/MyApplications.vue
+++ src/components/MyApplications.vue
@@ -28,64 +28,25 @@
       <template #[`item.actions`]="{ item }">
         <BtnEditDelete :item="item" @edit="editItem" @delete="deleteItem" />
       </template>
-
     </v-data-table>
 
-    <v-dialog v-model="dialog" max-width="600px">
-      <v-card>
-        <v-card-title>{{ dialogTitle }}</v-card-title>
-        <v-card-text>
-          <v-form ref="form">
-            <v-text-field v-model="editedItem.num" :rules="[rules.counter, rules.numIfNonEmpty]"
-              label="№ Заявки" required></v-text-field>
-            <v-text-field v-model="editedItem.stg" :rules="[rules.stgCheck, rules.max20Characters]" 
-              label="Продукт" required></v-text-field>
-            <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-menu v-model="menu2" :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
-                  offset-y min-width="auto">
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-text-field v-model="editedItem.dadd" label="Дата" prepend-icon="mdi-calendar" readonly
-                      v-bind="attrs" v-on="on"></v-text-field>
-                  </template>
-                  <v-date-picker v-model="editedItem.dadd" @input="menu2 = false"></v-date-picker>
-                </v-menu>
-              </v-col>
-            </v-row>
-            <v-text-field v-model="editedItem.client_name" :rules="[rules.client_nameCheck]" 
-               label="Клиент" required></v-text-field>
-            <v-row>
-              <v-col class="d-flex" cols="12" sm="6">
-                <v-select v-model="editedItem.state" :items="statusOptions" label="Статус"></v-select>
-              </v-col>
-            </v-row>
-            <v-text-field v-model="editedItem.person_phone" :rules="[rules.personPhoneCheck]" 
-              label="Телефон" required></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="closeDialog">Отмена</v-btn>
-          <v-btn color="blue darken-1" text @click="saveItem">Сохранить</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <DialogEditCreate :dialog.sync="dialog" :editedItem="editedItem" :dialogTitle="dialogTitle" @save="saveItem"
+      @close="closeDialog" />
   </div>
 </template>
-
 
 <script>
 import { mapMutations, mapState } from 'vuex';
 import moment from 'moment';
 import DialogDelete from './DialogDelete.vue';
 import BtnEditDelete from './BtnEditDelete.vue';
-
-
+import DialogEditCreate from './DialogEditCreate.vue';
 
 export default {
   components: {
     DialogDelete,
     BtnEditDelete,
+    DialogEditCreate,
   },
   data: () => ({
     menu2: false,
@@ -100,13 +61,6 @@ export default {
       { text: 'Статус', value: 'state' },
       { text: 'Телефон', value: 'person_phone' },
       { text: 'Действия', value: 'actions', sortable: false }
-    ],
-    statusOptions: [
-      'init',
-      'error',
-      'upload_docs',
-      'process',
-      'reupload_fls'
     ],
     editedIndex: -1,
     editedItem: {
@@ -125,30 +79,10 @@ export default {
       state: "init",
       person_phone: ""
     },
-    rules: {
-    counter: value => value.length <= 8 || 'Максимум 8 символов',
-    numIfNonEmpty: value => {
-      const pattern = /^[0-9]+$/;
-      return value === '' || pattern.test(value) || 'Недопустимый номер заявки. Используйте только цифры';
-    },
-    max20Characters: value => value.length <= 20 || 'Максимум 20 символов',
-    stgCheck: value => {
-      const pattern = /^[a-zA-Zа-яА-Я.'"0-9\s]+$/;
-      return value === '' || pattern.test(value) || 'Используйте цифры (0-9) или буквы (a-z, A-Z, а-я, А-Я). Максимум 20 символов.';
-    },
-    client_nameCheck: value => {
-      const pattern = /^[a-zA-Zа-яА-Я\s.'"0-9]+$/;
-      return value === '' || (value.length <= 30 && pattern.test(value)) || 'Используйте цифры (0-9) или буквы (a-z, A-Z, а-я, А-Я). Максимум 30 символов.';
-    },
-    personPhoneCheck: value => {
-      const pattern = /^[0-9]+$/;
-      return value === '' || (value.length === 11 && pattern.test(value)) || 'Недопустимый номер телефона. Должен содержать ровно 11 цифр.';
-    }
-  },
   }),
   watch: {
     dialog(val) {
-      val || this.close()
+      val || this.closeDialog()
     },
     dialogDelete(val) {
       val || this.closeDelete()
@@ -172,10 +106,11 @@ export default {
       };
     },
     selectOrder(orderId, num) {
-      this.$emit('select-order', {orderId, num});
-
+      this.$emit('select-order', { orderId, num });
     },
     openDialog() {
+      this.dialogTitle = "Создать заявку";
+      this.editedItem = Object.assign({}, this.defaultItem);
       this.dialog = true;
     },
     closeDialog() {
@@ -184,22 +119,14 @@ export default {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
       });
-      //this.$refs.form.reset();
     },
-    saveItem() {
-      if (this.$refs.form.validate()) {
-        if (!moment(this.editedItem.dadd).isValid()) {
-          this.editedItem.dadd = moment().format('YYYY-MM-DD HH:mm');
-        } else {
-          this.editedItem.dadd = moment(this.editedItem.dadd).format('YYYY-MM-DD HH:mm');
-        }
-        if (this.editedItem.id > -1) {
-          Object.assign(this.ordersList[this.editedIndex], this.editedItem);
-        } else {
-          this.addNewItem(this.editedItem);
-        }
-        this.closeDialog();
+    saveItem(localEditedItem) {
+      if (this.editedIndex > -1) {
+        Object.assign(this.ordersList[this.editedIndex], localEditedItem);
+      } else {
+        this.addNewItem(localEditedItem);
       }
+      this.closeDialog();
     },
     editItem(item) {
       this.editedIndex = this.ordersList.indexOf(item);
@@ -207,37 +134,27 @@ export default {
       this.dialogTitle = "Редактировать заявку";
       this.dialog = true;
     },
-
     deleteItem(item) {
       this.editedIndex = this.ordersList.indexOf(item);
       this.editedItem = Object.assign({}, item);
       this.dialogDelete = true;
     },
-
     deleteItemConfirm() {
       this.ordersList.splice(this.editedIndex, 1);
       this.closeDelete();
-    },
-    close() {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      });
     },
     closeDelete() {
       this.dialogDelete = false;
       this.$nextTick(() => {
         this.editedItem = Object.assign({}, this.defaultItem);
         this.editedIndex = -1;
-      })
+      });
     },
   },
   mounted() {
     this.$store.dispatch('fetchOrdersList');
   }
 };
-
 </script>
 
 <style scoped>
